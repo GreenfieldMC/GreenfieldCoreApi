8f1def2235106e0dad9f63359fe20a6090cf5f3c
NJDaeger <noahdaeger@hotmail.com>
2025-11-09 11:29:11 -0500

    Change Deleted column type from bit to tinyint

--- GreenfieldCoreDataAccess/Scripts/Tables/BuildCodes/init_BuildCodes_v1.0.sql
+++ GreenfieldCoreDataAccess/Scripts/Tables/BuildCodes/init_BuildCodes_v1.0.sql
@@ -3,6 +3,6 @@ create table if not exists BuildCodes (
     BuildCodeId bigint not null unique auto_increment primary key,
     ListOrder int not null,
     BuildCode nvarchar(4096) not null,
-    Deleted bit not null default 0,
+    Deleted tinyint(1) not null default 0,
     CreatedOn datetime default current_timestamp not null
 );
