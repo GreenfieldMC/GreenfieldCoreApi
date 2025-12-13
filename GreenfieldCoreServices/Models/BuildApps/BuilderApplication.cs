afc97817527dfe531ae2f111de5e9e84b97ac155
NJDaeger <noahdaeger@hotmail.com>
2025-12-13 11:27:20 -0500

    getters and setters on BuilderApplication

--- GreenfieldCoreServices/Models/BuildApps/BuilderApplication.cs
+++ GreenfieldCoreServices/Models/BuildApps/BuilderApplication.cs
@@ -2,15 +2,15 @@ namespace GreenfieldCoreServices.Models.BuildApps;
 
 public class BuilderApplication
 {
-    public required long ApplicationId;
-    public required List<BuildAppStatus> BuildAppStatuses;
-    public required long UserId;
-    public required int Age;
-    public string? Nationality;
-    public required List<BuildAppImage> HouseBuilds;
-    public required List<BuildAppImage> OtherBuilds;
-    public string? AdditionalBuildingInformation;
-    public required string WhyJoinGreenfield;
-    public string? AdditionalComments;
-    public required DateTime CreatedOn;
+    public required long ApplicationId { get; set; }
+    public required List<BuildAppStatus> BuildAppStatuses { get; set; }
+    public required long UserId { get; set; }
+    public required int Age { get; set; }
+    public string? Nationality { get; set; }
+    public required List<BuildAppImage> HouseBuilds { get; set; }
+    public required List<BuildAppImage> OtherBuilds { get; set; }
+    public string? AdditionalBuildingInformation { get; set; }
+    public required string WhyJoinGreenfield { get; set; }
+    public string? AdditionalComments { get; set; }
+    public required DateTime CreatedOn { get; set; }
 }
